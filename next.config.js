c09e7b3fa29a8ac94290dcc17bd9ebedb07372ef
Alexpavon16 <alejandropavonmake@gmail.com>
2025-02-26 14:27:58 +0100

    Corrección: Instaladas dependencias faltantes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    output: 'export', // Asegura que Next.js genera archivos estáticos
   };
   
   module.exports = nextConfig;
