18b7cb74097d8d785934e0d48b7f93b195561f5a
Alexpavon16 <alejandropavonmake@gmail.com>
2025-02-26 14:40:20 +0100

    Corrección: Configuración para generar archivos estáticos en out/

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const nextConfig = {
     reactStrictMode: true,
     output: 'export', // Asegura que Next.js genera archivos estáticos
-    distDir: 'out', // Cambia la carpeta de salida
   };
   
   module.exports = nextConfig;
