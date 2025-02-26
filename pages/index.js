7ad00ee8564f4614f1b58b0fb2dfa08c2e2d2b27
Alexpavon16 <alejandropavonmake@gmail.com>
2025-02-26 12:31:58 +0100

    Añadido archivo index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>¡Hola, el proyecto está en Vercel!</h1>
+        <p>Esta es una prueba inicial.</p>
+      </div>
+    );
+  }
+  
